1338d5f551abade163dcb24f1f2f99fa2d9022af
Pedro Holanda <pedroholanda@gmail.com>
2019-07-11 21:28:22 -0700

    try 10pow9

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -441,6 +441,21 @@ def run_prm():
 def run_prl():
     ALGORITHM_LIST = [ProgressiveRadixsortLSDCostModel]
     template_run(ALGORITHM_LIST,COLUMN_SIZE_LIST=[100000000],NUM_QUERIES=1000000)
+def run_adp_adp_idx_2():
+    ALGORITHM_LIST = [AdaptiveAdaptiveIndexing]
+    template_run(ALGORITHM_LIST,COLUMN_SIZE_LIST=[1000000000],NUM_QUERIES=1000000) 
+def run_pq_2():
+    ALGORITHM_LIST = [ProgressiveQuicksortCostModel]
+    template_run(ALGORITHM_LIST,COLUMN_SIZE_LIST=[1000000000],NUM_QUERIES=1000000)
+def run_pb_2():
+    ALGORITHM_LIST = [ProgressiveBucketsortEquiheightCostModel]
+    template_run(ALGORITHM_LIST,COLUMN_SIZE_LIST=[1000000000],NUM_QUERIES=1000000)
+def run_prm_2():
+    ALGORITHM_LIST = [ProgressiveRadixsortMSDCostModel]
+    template_run(ALGORITHM_LIST,COLUMN_SIZE_LIST=[1000000000],NUM_QUERIES=1000000)
+def run_prl_2():
+    ALGORITHM_LIST = [ProgressiveRadixsortLSDCostModel]
+    template_run(ALGORITHM_LIST,COLUMN_SIZE_LIST=[1000000000],NUM_QUERIES=1000000)
 
 def run_prm_cm():
     ALGORITHM_LIST = [ProgressiveRadixsortMSDCostModel]
@@ -461,19 +476,15 @@ def run_prm_cm():
 #run_adp_adp_idx()
 #stones07
 #run_progressive_fixed_deltas()
-
-#stones06
-#run_pq()
-#stones07
-#run_pb()
 #stones09
-#run_prm()
+#run_pq_2()
 #stones10
-#run_prl()
+#run_pb_2()
 #stones11
-#run_skyserver_baseline()
+#run_prm_2()
 #stones12
-#run_skyserver_progressive_cost_model()
-# run_prm_cm()
-# run_adp_adp_idx()
+#run_prl_2()
+#stones13
+#run_adp_adp_idx_2()
+
 db.close()
